9ef27faf8cf58729f0be41a0e5445c2b8f443a42
Rem2525 <remmehyar@gmail.com>
2026-07-27 13:46:05 +0300

    Added currency api  to config file

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 SERPAPI_API_KEY = os.getenv("SERPAPI_API_KEY")
+EXCHANGERATE_API_KEY = os.getenv("EXCHANGERATE_API_KEY")
 
 OPENAI_MODEL = "gpt-4.1-mini"
